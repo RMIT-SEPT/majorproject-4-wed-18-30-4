da4eeb931750a0701ad01e12dddebf516f166cae
MinuraIddamalgoda <mini2476@gmail.com>
2020-09-22 19:56:46 +1000

    Remove unused code

--- BackEnd/swagger/gen_contract.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-args="localhost:8080/v3/api-docs/"
-if curl --fail "$args" > /dev/null 2>&1; then
-  curl "$args" | jq > booking_api.json
-else
-  echo "Booking API not running at localhost:8080" >2
-fi
-
